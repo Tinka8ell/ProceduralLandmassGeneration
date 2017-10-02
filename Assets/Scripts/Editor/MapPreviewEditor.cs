5d89e8f724108a8d709ad346f6dacdea0f2b1cdb
Featrem <antoinebruget@gmail.com>
2017-10-02 17:37:19 +0200

    Initial commit

---
+++ Assets/Scripts/Editor/MapPreviewEditor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor (typeof (MapPreview))]
+public class MapPreviewEditor : Editor {
+
+	public override void OnInspectorGUI() {
+        MapPreview mapGen = (MapPreview)target;
+
+		if (DrawDefaultInspector ()) {
+			if (mapGen.autoUpdate) {
+				mapGen.DrawMapInEditor ();
+			}
+		}
+
+		if (GUILayout.Button ("Generate")) {
+			mapGen.DrawMapInEditor();
+		}
+	}
+}
